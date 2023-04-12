7e9abed496680f39d2f54c6b63c66f170ed26659
Mike Wood <mikewwood@icloud.com>
2023-04-12 19:26:48 -0400

    add places to visit.

--- friends.rb
+++ friends.rb
@@ -3,3 +3,7 @@ friend_names = ["Bob", "Shirley", "Sue", "Tamara", "Mike"]
 p friend_names[2]
 p friend_names[4]
 p friend_names[0]
+
+places = ["Israel", "Brazil", "Austin", "Cancun", "Vietnam"]
+
+p places.push("Australia")
